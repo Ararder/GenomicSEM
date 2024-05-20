7c6641ad2a7f67051aef45c216d639618417b5fb
Arvid Harder <48621063+Ararder@users.noreply.github.com>
2024-05-20 15:43:47 +0200

    Update enrich.R

--- R/enrich.R
+++ R/enrich.R
@@ -520,9 +520,8 @@ enrich <-function(s_covstruc, model = "",params,fix= "regressions",std.lv=FALSE,
               results$SE<-NULL
               results$error<-ifelse(class(part_warn$value) == "lavaan", 0, as.character(part_warn$value$message))[1]
               results$warning<-ifelse(class(part_warn$warning) == 'NULL', 0, as.character(part_warn$warning$message))[1]
-              
+              Results_List<-vector(mode="list",length=nrow(results))
               if(n == 1){
-                Results_List<-vector(mode="list",length=nrow(results))
                 for(y in 1:nrow(results)){
                   Results_List[[y]]<-as.data.frame(matrix(NA,ncol=ncol(results),nrow=length(s_covstruc$S)))
                   colnames(Results_List[[y]])<-c("Annotation", "lhs", "op", "rhs", "Cov_Smooth", "Z_smooth", "Enrichment", "Enrichment_SE", "Enrichment_p_value", "Error", "Warning")
